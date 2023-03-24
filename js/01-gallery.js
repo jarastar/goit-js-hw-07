c9ddbd6b3643614ccd5406690de4e91b81358921
Astar248 <taras.y248@gmail.com>
2023-03-24 03:19:32 +0200

    hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryMarkup(galleryItems) {
+  let galleryMarkup = '';
+  for (let item of galleryItems) {
+    const { preview, original, description } = item;
+    galleryMarkup += `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+  }
+  return galleryMarkup;
+}
+
+galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
+
+galleryContainer.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (event.target.nodeName === 'IMG') {
+    const originalSrc = event.target.dataset.source;
+    const instance = basicLightbox.create(`
+      <img src="${originalSrc}" alt="${event.target.alt}">
+    `);
+    instance.show();
+
+    const escapeHandler = (event) => {
+      if (event.code === 'Escape') {
+        instance.close();
+        document.removeEventListener('keydown', escapeHandler);
+      }
+    };
+    document.addEventListener('keydown', escapeHandler);
+  }
+});
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.innerHTML = galleryItems.reduce((acc, { preview, original, description }) =>
+  acc + `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}">
+      </a>
+    </li>
+  `, '');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+
 console.log(galleryItems);
+
+
